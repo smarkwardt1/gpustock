155cd685c25f0e23f6147058f2f72d6b177fdcbe
smarkwardt <markwami@oregonstate.du>
2021-11-08 13:52:25 -0600

    connected with microservice

--- server.js
+++ server.js
@@ -1,20 +1,39 @@
-'use strict';
+"use strict";
 
-const PORT = 3000;
+const PORT = 4000;
 
 const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require('axios');
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.urlencoded({
-    extended: true
-}));
-
-app.use(express.static('public'));
+app.use(express.static("public"));
 // Note: Don't add or change anything above this line.
 
+app.post("/", (req, res) => {
+    const search_value = req.body.model;
+    const threshold = req.body.threshold;
+    console.log("Search Value: " + JSON.stringify(search_value));
+    console.log("Threshold: " + JSON.stringify(threshold));
+
+    let search_url = 'http://localhost:3000/pricing/' + search_value;
+    console.log("Search URL: " + search_url)
+    
+    axios.get(search_url)
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+});
 
 // Note: Don't add or change anything below this line.
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}...`);
+  console.log(`Server listening on port ${PORT}...`);
 });

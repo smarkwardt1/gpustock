3fffe103208ab2c1ddc3145286c1c426b5e8373f
smarkwardt <markwami@oregonstate.du>
2021-11-30 12:33:51 -0600

    table sort working

--- public/buildtable.js
+++ public/buildtable.js
@@ -1,19 +1,19 @@
 fetch("./data/search_data.json")
-.then((response) => response.json())
-.then((data) => {
-  buildTable(data);
-});
+  .then((response) => response.json())
+  .then((data) => {
+    buildTable(data);
+  });
 
 function buildTable(data) {
-var table = document.getElementById("table-data");
+  var table = document.getElementById("table-data");
 
-for (var i = 0; i < data.length; i++) {
-  var row = `<tr>
+  for (var i = 0; i < data.length; i++) {
+    var row = `<tr>
                     <td>${data[i].store}</td>
                     <td>${data[i].name}</td>
         <td><a href="${data[i].url}" target="_blank"><img src="./images/link.png" alt="Link Icon"></a></td>
       <td id="price">${data[i].price}</td>
               </tr>`;
-  table.innerHTML += row;
-}
+    table.innerHTML += row;
+  }
 }
